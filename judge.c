162270f6143324edb8e2bbd1c57953bbbfd2ee96
yukatan1701 <yukki.lapenko@gmail.com>
2018-11-15 16:51:55 +0600

    Test call added

--- judge.c
+++ judge.c
@@ -1,3 +1,4 @@
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,18 +32,23 @@ void print_settings(Settings set)
 	printf("score: %s\n", set.score);
 }
 
+char *copy_string(char *value)
+{
+	int len = strlen(value);
+	char *score = malloc(sizeof(char) * (len + 1));
+	if (score == NULL)
+		allocation_error();
+	memset(score, 0, len + 1);
+	memcpy(score, value, len);
+	return score;
+}
+
 void put_data(Settings *set, char *key, char *value)
 {
 	if (strcmp(key, "problems") == 0) {
 		(*set).problems = atoi(value);
 	} else if (strcmp(key, "score") == 0) {
-		int len = strlen(value);
-		char *score = malloc(sizeof(char) * (len + 1));
-		if (score == NULL)
-			allocation_error();
-		memset(score, 0, len + 1);
-		memcpy(score, value, len);
-		(*set).score = score;
+		(*set).score = copy_string(value);
 	}
 }
 
@@ -152,6 +158,86 @@ void print_user_list(char **user_list)
 		puts(*cur);
 }
 
+char *generate_file_path(char *contest_name, char *username, char letter)
+{
+	static const int filename_len = 3;
+	static const int slashes_count = 2;
+	int user_path_len = strlen(contest_name) + strlen(username) + 
+		filename_len + slashes_count;
+	char *user_file_path = malloc(sizeof(char) * (user_path_len + 1));
+	if (user_file_path == NULL)
+		allocation_error();
+	memset(user_file_path, 0, user_path_len + 1);
+	strcpy(user_file_path, contest_name);
+	strcat(user_file_path, "/");
+	strcat(user_file_path, username);
+	strcat(user_file_path, "/");
+	strncat(user_file_path, &letter, 1);
+	strcat(user_file_path, ".c");
+	return user_file_path;
+}
+
+char *generate_output_path(char *contest_name, char letter)
+{	
+	static const char test_dir[] = "tests";
+	static const int slashes_count = 2;
+	int tests_path_len = strlen(contest_name) + strlen(test_dir) + slashes_count + 1;
+	char *output_path = malloc(sizeof(char) * (tests_path_len + 1));
+	if (output_path == NULL)
+		allocation_error();
+	memset(output_path, 0, tests_path_len + 1);
+	strcpy(output_path, contest_name);
+	strcat(output_path, "/");
+	strcat(output_path, test_dir);
+	strcat(output_path, "/");
+	strncat(output_path, &letter, 1);
+	return output_path;
+}
+
+void free_buffer()
+{
+	char ch;
+	while ((ch = fgetc(stdin)) != EOF)
+		;
+}
+
+void run_tests(char *contest_name, char **user_list, Settings set)
+{
+	char *results = malloc(sizeof(char) * (set.problems) + 1);
+	if (results == NULL)
+		allocation_error();
+	const char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (char **cur_user = user_list; *cur_user; cur_user++) {
+		for (int i = 0; i < set.problems && i < sizeof(alphabet) - 1; i++) {
+			char *user_file_path = generate_file_path(contest_name, *cur_user, alphabet[i]);
+			char *output_path = generate_output_path(contest_name, alphabet[i]);
+			int fd[2];
+			pipe(fd);
+			if (fork() == 0)
+			{
+				close(fd[0]);
+				dup2(fd[1], 1);
+				close(fd[1]);
+				execl("test_tmp", user_file_path, output_path, NULL);
+			}
+			close(fd[1]);
+			dup2(fd[0], 0);
+			close(fd[0]);
+			int status = 0;
+			wait(&status);
+
+			memset(results, 0, set.problems + 1);
+			fgets(results, set.problems, stdin);
+			puts(results);
+			free_buffer();
+			/*puts(user_file_path);
+			puts(output_path);
+			puts("------------");*/
+		}
+	}
+	free(results);
+}
+
 int main(int argc, char *args[])
 {
 	char *contest_name = args[1];
@@ -163,7 +249,8 @@ int main(int argc, char *args[])
 	print_settings(set);
 	char **user_list = get_user_list();
 	print_user_list(user_list);
-	//free_settings();
-	//free_user_list();
+	run_tests(contest_name, user_list, set);
+	//TODO: free_settings();
+	//TODO: free_user_list();
 	return 0;
 }
